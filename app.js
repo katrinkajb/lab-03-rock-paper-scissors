a7f09d4bafcf90da9f0517f295e967eb02eaf06a
katrinkajb@gmail.com <katrinkajb@gmail.com>
2021-01-13 15:49:55 -0800

    saving changes before I try a drastic change

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const shootButton = document.getElementById('shoot-button');
 const computerResult = document.getElementById('computer-result');
 const resultsArea = document.getElementById('results-area');
 const winsCount = document.getElementById('wins-count');
-const totalGamesCount = document.getElementById('total-games-coun');
+const lossesCount = document.getElementById('losses-count');
+const totalGamesCount = document.getElementById('total-games-count');
 
 
 // initialize state
@@ -22,17 +23,25 @@ shootButton.addEventListener('click', () => {
 //     -Set up a function to randomly pick between rock, scissors, and paper
     const computerRockPaperScissors = rockPaperScissorsFunc(computerThrow);
 
+    console.log(computerRockPaperScissors);
 // b) Store user's throw
 //     -grab selected input and get its value
 const radioSelection = document.querySelector('input[type="radio"]:checked');
 const userChoice = radioSelection.value;
 
 // c) Compare user choice and computer throw. Write a function
-   
-// d) Display results of throw
-
+    const winner = determineWinner(userChoice, computerRockPaperScissors);
+    console.log(winner);
+// d) Display results of throw.
+    updateResults();
 })
 
+function updateResults() {
+    // resultsArea.textContent = winner; NEED TO DO IN FUNCTION
+    winsCount.textContent = wins;
+    lossesCount.textContent = total - wins;
+    totalGamesCount.textContent = total;
+}
 
 
 
