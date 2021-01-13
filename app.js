dc15c00531758e92f51235de88582139b34dcb41
katrinkajb@gmail.com <katrinkajb@gmail.com>
2021-01-13 14:06:33 -0800

    initial html and css setup as well as beginning JS setup

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 // import functions and grab DOM elements
 
+const shootButton = document.getElementById('shoot-button');
+const computerThrow = document.getElementById('computer-throw');
+const resultsArea = document.getElementById('results-area');
+const winsCount = document.getElementById('wins-count');
+const totalGamesCount = document.getElementById('total-games-coun');
+
+
 // initialize state
+let wins = 0;
+let total = 0;
 
 // set event listeners to update state and DOM
+
+shootButton.addEventListener('click', () => {
+    
+
+})
